6e3df81bbc34242319f48ac38f124609018b9dbb
Vitor Sam galaxy <vitorcarvalhosampaio@hotmail.com>
2023-08-15 15:00:00 -0300

    unindo as branches

---
+++ src/scraping/data_scraper.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pandas as pd
+import time
+import warnings
+import re
+import os
+import yaml
+
+warnings.filterwarnings('ignore')
+
+class DataScraper:
+
+    def __init__(self, config):
+        self.links_filename = os.path.join('data', config['scraper']['links_filename'])
+        self.detailed_filename = os.path.join('data', config['scraper']['detailed_filename'])
+        self.sleep_time = config['scraper']['sleep_time']
+        self.options = Options()
+        self.options.add_argument("--start-maximized")
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--ignore-ssl-errors')
+        self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(options=self.options)
+        self.error_count = 0
+
+    def fetch_data_from_ad(self, url, region):
+        try:
+            self.driver.get(url)
+            WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1[class="ad__sc-45jt43-0 htAiPK sc-hSdWYo bYQcLm"]')))
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+
+            # Your existing scraping logic
+            all_spans = soup.find_all('span', {'class': 'ad__sc-1f2ug0x-1 cpGpXB sc-hSdWYo gwYTWo'})
+            details = soup.find('span', text=re.compile('Detalhes do imóvel'))
+            rooms_elem = soup.find('span', text='Quartos')
+            rooms_no = rooms_elem.find_next_sibling('div').a.text if rooms_elem and rooms_elem.find_next_sibling('div') else 'N/A'
+            price_text = soup.find_all('h2', {'class': 'ad__sc-12l420o-1 haeKsn sc-hSdWYo grMlBs'})[1].text.replace('.', '')
+            price = int(price_text) if price_text != 'N/A' else None
+
+            data = {
+                'TITLE': soup.find('h1', {'class': 'ad__sc-45jt43-0 htAiPK sc-hSdWYo bYQcLm'}).text,
+                'LINK': url,
+                'DATE_SCRAPE': datetime.today().strftime('%Y-%m-%d'),
+                'CEP': all_spans[0].text if len(all_spans) > 0 else 'N/A',
+                'CITY': all_spans[1].text if len(all_spans) > 1 else 'N/A',
+                'NEIGHBORHOOD': all_spans[2].text if len(all_spans) > 2 else 'N/A',
+                'CONDO': all_spans[4].text if len(all_spans) > 4 else 'N/A',
+                'TAX': all_spans[5].text if len(all_spans) > 5 else 'N/A',
+                'AREA': all_spans[6].text if len(all_spans) > 6 else 'N/A',
+                'ROOMS_NO': rooms_no,
+                'BATH_NO': all_spans[7].text if len(all_spans) > 7 else 'N/A',
+                'PARKING_SPOTS': all_spans[8].text if len(all_spans) > 8 else 'N/A',
+                'APARTMENT_DETAILS': details.find_next_sibling().text if details and details.find_next_sibling() else 'N/A',
+                'PRICE': price,
+                'REGION': region
+            }
+            return data
+
+        except TimeoutException:
+            print(f"TimeoutException occurred for URL: {url}. Moving on to the next link.")
+            self.error_count += 1
+            return {}
+
+    def scrape_data(self):
+        # Load links from CSV
+        df_links = pd.read_csv(self.links_filename)
+
+        all_data = []
+
+        for _, row in df_links.iterrows():
+            url = row['link']
+            region = row['REGION']
+
+            data = self.fetch_data_from_ad(url, region)
+            if data:
+                all_data.append(data)  # Use a list to store data
+
+            time.sleep(self.sleep_time)
+
+        df_detailed = pd.DataFrame(all_data)  # Convert list to DataFrame
+        df_detailed.to_csv(self.detailed_filename, index=False)
+
+    def quit_driver(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    scraper = DataScraper(config)
+    scraper.scrape_data()
+    scraper.quit_driver()
+    print(f"Scraping completed with {scraper.error_count} errors.")
--- src/scraping/link_scraper.py
+++ src/scraping/link_scraper.py
@@ -13,67 +13,70 @@ import os
 class LinkScraper:
     
     def __init__(self, config):
-        self.start_url = config['scraper']['start_url']
+        self.regions = config['scraper']['regions']
         self.csv_filename = config['scraper']['csv_filename']
         self.sleep_time = config['scraper']['sleep_time']
         self.options = Options()
         self.options.add_argument("--start-maximized")
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--ignore-ssl-errors')
+        self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(options=self.options)
+        self.seen_links = set()
         
     def fetch_data_from_page(self, url):
         self.driver.get(url)
         WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h2[class="horizontal title sc-ifAKCX hUnWqk"]')))
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-        titles = soup.find_all('h2', attrs={'class': 'horizontal title sc-ifAKCX hUnWqk'})
         links = soup.find_all('a', attrs={'data-ds-component': 'DS-NewAdCard-Link'})
         dates = soup.find_all('p', attrs={'data-ds-component': 'DS-Text', 'data-testid': 'ds-adcard-date'})
 
         data = []
 
+        # Coletar todos os anúncios da página
+        for link, date in zip(links, dates):
+            if "Hoje" in date.text and link['href'] not in self.seen_links:
+                self.seen_links.add(link['href'])  # Adicione o link ao conjunto de links vistos
+                row = {
+                    'link': link['href'],
+                    'date': datetime.now()
+                }
+                data.append(row)
+
+
         # Ignorar os 4 primeiros anúncios impulsionados
-        for title, link, date in zip(titles[4:], links[4:], dates[4:]):
-            if "Ontem" in date.text:
-                return None
-            
-            row = {
-                'titulo': title.text,
-                'link': link['href'],
-                'date': datetime.now()
-            }
+        return data[4:]
 
-            data.append(row)
-
-        return data
-
-
-    def save_to_csv(self, data, mode='w'):
+    def save_to_csv(self, data, region, mode='w'):
         df = pd.DataFrame(data)
+        df['REGION'] = region
         df.to_csv(self.csv_filename, mode=mode, header=not os.path.exists(self.csv_filename), index=False)
-        
+
     def scrape_links(self):
-        page = 1
+        for region in self.regions:
+            page = 1
+            region_name = region['name']
+            base_url = region['url']
+            while True:
+                if page == 1:
+                    url = base_url
+                else:
+                    url = f'{base_url}&o={page}'
 
-        while True:
-            if page == 1:
-                url = f'{self.start_url}?sf=1'
-            else:
-                url = f'{self.start_url}?o={page}&sf=1'
-            
-            new_data = self.fetch_data_from_page(url)
-            
-            if not new_data:  # Se encontrarmos um anúncio de "Ontem"
-                print("An ad from 'Yesterday' found. Exiting scraper.")
-                break
+                new_data = self.fetch_data_from_page(url)
+                
+                if not new_data:  # Break if we find an ad with "Ontem" or the page doesn't contain more data
+                    break
 
-            print(f"Page number: {page}")
-            print(f"Length of DataFrame: {len(new_data)}")
+                print(f"Scraping region: {region_name} - Page number: {page}")
+                print(f"Length of DataFrame: {len(new_data)}")
 
-            # Save after every page
-            self.save_to_csv(new_data, mode='a')
+                # Save after every page
+                self.save_to_csv(new_data, region_name, mode='a')
 
-            page += 1
-            time.sleep(self.sleep_time)  # Pause for a few seconds between requests
+                page += 1
+                time.sleep(self.sleep_time)  # Pause for a few seconds between requests
 
         self.driver.quit()
 
