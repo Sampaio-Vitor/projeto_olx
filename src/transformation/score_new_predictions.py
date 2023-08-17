5ee5656bb6d475297aff6f15eba244e87e80e804
Vitor Sampaio <110466124+Sampaio-Vitor@users.noreply.github.com>
2023-08-17 15:17:31 +0000

    "pipelines working"

--- src/transformation/score_new_predictions.py
+++ src/transformation/score_new_predictions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from joblib import load
 from src.utils.utils import load_config
+import numpy as np
 
 config = load_config("config.yaml")
 features_predict  = config['model']['features']
@@ -13,15 +14,16 @@ if __name__ == "__main__":
     
     # Apply the pipeline to preprocess the new data
     preprocessed_data = preprocessing_pipeline.transform(new_data)
-    nan_counts_by_region = preprocessed_data[preprocessed_data['LOG_AVG_PRICE_PER_SQMT_BY_REGION'].isna()]['REGION'].value_counts()
-    print(nan_counts_by_region)
+
 
     # Now, score the preprocessed data
     predictions = trained_model.predict(preprocessed_data[features_predict])
-    
-    # You can add these predictions to your preprocessed_data dataframe or save them separately.
-    # This line appends predictions as a new column:
-    preprocessed_data['Predictions'] = predictions
+
+    # Convert predictions from log scale to linear scale
+    predictions_linear = np.exp(predictions)
+
+    # Add the predictions in linear scale to your DataFrame
+    preprocessed_data['Predictions'] = predictions_linear
     
     # Save or process scored data as needed
     preprocessed_data.to_csv("data/scored_data.csv", index=False)
